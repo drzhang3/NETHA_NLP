260932c9a6d7eef85ceabf0cceb5c496ae542420
drzhang3 <1911470500@qq.com>
2020-03-05 20:56:36 +0800

    update

--- advers.py
+++ advers.py
@@ -49,7 +49,7 @@ alpha = 0.6 # 对抗性权重
 # checkpoint_path = 'NEZHA/model.ckpt-900000'
 # dict_path = 'NEZHA/vocab.txt'
 config_path = 'BERT_wwm/bert_config.json'
-checkpoint_path = 'BERT_wwm/bert_model.ckpt.data-00000-of-00001'
+checkpoint_path = 'BERT_wwm/bert_model.ckpt'
 dict_path = 'BERT_wwm/vocab.txt'
 
 
