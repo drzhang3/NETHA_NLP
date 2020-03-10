35782366d29a6b0cd618e788a5f36c7ec8f50d21
drzhang3 <1911470500@qq.com>
2020-03-10 20:26:37 +0800

    update

--- single.py
+++ single.py
@@ -91,7 +91,7 @@ bert = build_bert_model(
     return_keras_model=False,
 )    
 
-# output = Dropout(rate=0.01)(bert.model.output)
+output = Dropout(rate=0.01)(bert.model.output)
 output = Dense(units=2,
                activation='softmax',
                kernel_initializer=bert.initializer)(bert.model.output)
